ed3cb06fecc0bb5c91b743609c5b5cff4bb3f687
siarasmall <siarasmall19@gmail.com>
2018-07-13 15:24:36 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,17 @@
 # river.rb
+class River
+  attr_accessor :name, :length, :countries, :discharge
+  
+  def initialize(name)
+    @name = name
+  end #defini
+  
+  def flood
+    @discharge = @discharge.to_i * 1.3
+  end 
+  
+  def dry_up
+    @discharge = @discharge / 2
+  end 
+    
+end 
