2dba0e812336cc1dcd2c243c553d889467b1a3d7
siarasmall <siarasmall19@gmail.com>
2018-07-13 15:19:17 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,13 @@
 # river.rb
+class Rivers
+  attr_writer :name, :length, :countries, :discharge
+  
+  def initialize(name)
+    @name = name
+  end #defini
+  
+  def flood
+    @discharge = @dishcharge. to_i * 1.3
+  end 
+    
+end 
